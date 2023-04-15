047ee845437440427583a0479bddfa4c3fafda4c
Leandro Yabut <leandro.yabut@smoothstack.com>
2023-04-14 19:39:40 -0600

    HM-19 Add membership status field.

--- src/main/java/com/laarc/hoamanagerserver/model/Membership.java
+++ src/main/java/com/laarc/hoamanagerserver/model/Membership.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
--- src/main/java/com/laarc/hoamanagerserver/model/User.java
+++ src/main/java/com/laarc/hoamanagerserver/model/User.java
@@ -2,6 +2,8 @@ package com.laarc.hoamanagerserver.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,9 +16,12 @@ import lombok.Setter;
 @Setter
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+
     @NotNull
     @Column(unique = true)
-    private String userId;
+    private String email;
     @ManyToOne
     @JoinColumn(name = "user_role_id", nullable = false)
     private UserRole userRole;
