34b3e56c52200122263879b25eca1caa76f6cdd8
Leandro Yabut <leandro.yabut@smoothstack.com>
2023-04-18 21:52:32 -0600

    HM-19 Find user by user id.

--- src/main/java/com/laarc/hoamanagerserver/api/module/user/service/UserService.java
+++ src/main/java/com/laarc/hoamanagerserver/api/module/user/service/UserService.java
@@ -1,7 +1,20 @@
 package com.laarc.hoamanagerserver.api.module.user.service;
 
+import com.laarc.hoamanagerserver.exception.UserNotFoundException;
+import com.laarc.hoamanagerserver.shared.model.User;
+import com.laarc.hoamanagerserver.shared.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
 }
