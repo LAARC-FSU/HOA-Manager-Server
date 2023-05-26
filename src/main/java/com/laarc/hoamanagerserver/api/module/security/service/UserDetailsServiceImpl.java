6721637dd3d865c0105d9ee8634617012c8bcc67
Leandro Yabut <leandro.yabut@smoothstack.com>
2023-05-25 23:11:55 -0600

    HM-92 Add user details service implementation.

--- src/main/java/com/laarc/hoamanagerserver/api/module/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/laarc/hoamanagerserver/api/module/security/service/UserDetailsServiceImpl.java
@@ -1,15 +1,23 @@
 package com.laarc.hoamanagerserver.api.module.security.service;
 
+import com.laarc.hoamanagerserver.api.module.user.repository.UserRepository;
+import com.laarc.hoamanagerserver.exception.http.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email).orElseThrow(
+                () -> new UnauthorizedException("User with email " + email + " does not exist.")
+        );
     }
 }
